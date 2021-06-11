2e41a323641630c3ab98b8913c5693f1b0864ae6
hybrid <nikolapantelic434@gmail.com>
2021-06-11 19:27:33 +0200

    Initial project initialization

---
+++ src/main/java/com/github/nikolapantelicftn/weatherstatsbackend/WeatherStatsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.nikolapantelicftn.weatherstatsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherStatsBackendApplication.class, args);
+	}
+
+}
